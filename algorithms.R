d1e6253c6226611d69a72e7614da9c7e084414d8
alex-d13 <dietrich.alexander1310@gmail.com>
2019-12-12 16:17:21 +0100

    Algorthm works now, but slow

--- algorithms.R
+++ algorithms.R
@@ -2,7 +2,8 @@
 # store functions for different methods of calculating co-occurence
 ############
 
-#file = "Documents/Studium_Bioinformatik/Master/Semester1/SystemsBioMedicine/Project/namco/namco/OTUs_Table-norm.tab"
+file = "testdata/OTUs_Table-norm.tab"
+file2 = "testdata/gut_16s_abundance.txt"
 
 library(data.table)
 library(tidyr)
@@ -39,77 +40,80 @@ basic_approach <- function(file){
 
 
 basic_approach_flipped <- function(file){
+  print(Sys.time())
+  options(stringsAsFactors = F)
+  #read in file (flip and change first column to column names -> this needs to be adressed differently for type of OTU-table)
   table1 <- as.data.table(t(as.matrix(read.table(file = file,
                                       sep = "\t",
                                       dec = ".",
                                       header = T,
                                       row.names = 1))))
   
+  #save OTU names
   OTUs <- colnames(table1)
   sample_size <- nrow(table1)
   
-  cutoff <- 0
+  #cutoff: take 0.1 percentile of each sample and then mean over all samples to find cutoff; values below are considered to be 0
+  cutoff <- mean(t(table1[, apply(table1,1,quantile,probs =.1,na.rm=TRUE)] ))
   
   #binarization & normalization
   #cutoff nicht bei 1, sondern percentile wise...
   table1[, (OTUs) := lapply(.SD,function(x){ ifelse(x>cutoff,1,0)}),.SDcols =OTUs][
     ,(OTUs) := lapply(.SD,function(x){return(x/sample_size)}),.SDcols =OTUs]
   
-  
-  #sums <- colSums(table1)
-  
-  #m <- as.matrix(table1)
-  #make into for loop -> only get half of combinations w/o identity!
-  result <- as.data.table(expand.grid(OTU1=OTUs,OTU2=OTUs))
+  result <- unique_combinations(OTUs,OTUs)
   
   counts <- unlist(lapply(seq(1,nrow(result)), function(x){
+    #find 2 OTUs, for which value is calculated
     otu1 <- unlist(result[x,1])
     otu2 <- unlist(result[x,2])
     
-    total_value = 0
+    #skip identical OTUs
+    # -> can't skip, otherwise there are not enough values in counts list in order to merge with result dt
+    if(otu1==otu2){
+      #result[OTU1 == otu1 & OTU2 == otu2] <- NULL
+      return (0)
+    }
     
-    l<-unlist(lapply(seq(1,nrow(table1)),function(y){
+    #add OTU values for 2 OTUs for each sample, where they both appear
+    l<-unlist(sapply(seq(1,nrow(table1)),function(y){
       otu1_value <- table1[[otu1]][y]
       otu2_value <- table1[[otu2]][y]
       if(otu1_value > 0 & otu2_value > 0){
         s <- otu1_value + otu2_value
-        total_value <- total_value + s
-        return(total_value)
+        return(s)
       }else{
         return (NULL)
       }
     }))
     
-    
-    
-    # otu1_value <- table1[[otu1]][x]
-    # otu2_value <- table1[[otu2]][x]
-    # print(paste("1: ",otu1_value,x,otu1))
-    # print(paste("2: ",otu2_value,x,otu2))
-    # if(otu1_value > 0 & otu2_value > 0){
-    #   value <- otu1_value+otu2_value
-    #   return(value)
-    # }else{
-    #   return (0)
-    # }
+    total <- Reduce(`+`,l)
+    #debugging stuff
+    if(x%%1000==0){
+      print(x)
+    }
+    #return count value
+    return(total)
+
   }))
-  
-  
-  # for(i in seq(1,nrow(result))){
-  #   
-  #   otu1 <- result$OTU1[i]
-  #   otu2 <- result$OTU2[i]
-  #   value <- sums[otu1]+sums[otu2]
-  #   #print(value)
-  #   counts<-append(counts,value)
-  #   
-  # }
-  
+
+  #merge count list to data table; order is consistent
   result[,counts:=counts]
+  print(Sys.time())
+  return(result)
+}
+
+#create data table with all unique combinations, where (A,B) == (B,A)
+unique_combinations <- function(l1, l2){
+  t<-as.data.table(expand.grid(l1,l2))
+  t$key<-apply(t, 1, function(x)paste(sort(x), collapse=''))
+  t<-subset(t, !duplicated(t$key))
+  t$key<-NULL
+  names(t)<-c("OTU1","OTU2")
+  return(t)
 }
 
 
-
-basic_approach_flipped(file)
+basic_approach_flipped(file2)
 
 
