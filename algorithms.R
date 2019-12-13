a9565cc3dc7b559fdecf122e337235e6c7520970
alex-d13 <dietrich.alexander1310@gmail.com>
2019-12-13 12:08:11 +0100

    improved algorithm and speed

--- algorithms.R
+++ algorithms.R
@@ -7,38 +7,57 @@ file2 = "testdata/gut_16s_abundance.txt"
 
 library(data.table)
 library(tidyr)
+library(reshape2)
 
 basic_approach <- function(file){
-  #TODO: test file to be a valid OTU table
-  
-  
-  table <- fread(file = file,
-                 sep = "\t",
-                 dec = ".")
-  
-  #getting table into right format (row == sample, column == OTU)
-    #as.data.table(t(as.matrix(read.table(file = file,
-    #                      sep = "\t",
-    #                      dec = ".",
-    #                      header = T,
-    #                      row.names = 1))))
+  print(Sys.time())
+  table1 <- as.data.table(t(as.matrix(read.table(file = file,
+                                                 sep = "\t",
+                                                 dec = ".",
+                                                 header = T,
+                                                 row.names = 1))))
   
   #save OTU names
-  OTUs <- as.list(table[,1])
-  table[,V1 := NULL]  
+  OTUs <- colnames(table1)
+  n_otus <- length(OTUs)
+  sample_size <- nrow(table1)
   
-  
-  cols <- colnames(table)
-  sample_size <- nrow(table)
+  #cutoff: take 0.1 percentile of each sample and then mean over all samples to find cutoff; values below are considered to be 0
+  cutoff <- mean(t(table1[, apply(table1,1,quantile,probs =.1,na.rm=TRUE)] ))
   
   #binarization & normalization
-  table[,(cols) := lapply(.SD,function(x){ ifelse(x > 0, 1, 0)}),.SDcols = cols][
-    ,(cols) := lapply(.SD,function(x) {return (x*100/sample_size)}),.SDcols = cols]
+  #cutoff nicht bei 1, sondern percentile wise...
+  table1[, (OTUs) := lapply(.SD,function(x){ ifelse(x>cutoff,1,0)}),.SDcols =OTUs][
+    ,(OTUs) := lapply(.SD,function(x){return(x/sample_size)}),.SDcols =OTUs]
   
   
+  mat <- matrix(,nrow=n_otus,ncol=n_otus)
+  table1<-as.data.frame(table1)
+  
+  for(i in 1:n_otus){
+    rowsToCount <- which(table1[,i] > 0)
+    for(j in 1:n_otus){
+      #this skips upper triangle
+      if(i<j){
+        next()
+      }else{
+        mat[i,j]<-sum(table1[rowsToCount,j])
+      }
+    }
+    #set diagonale to NA
+    mat[i,i]<-NA
+  }
+  rownames(mat)<-OTUs
+  colnames(mat)<-OTUs
+  
+  counts<-setNames(melt(mat,na.rm = T),c("OTU1","OTU2","value"))
+  print(Sys.time())
+  return(counts)
 }
 
-
+####deprecated#####
+#DO NOT USE!!
+#..only if you have a lot of time 
 basic_approach_flipped <- function(file){
   print(Sys.time())
   options(stringsAsFactors = F)
@@ -80,10 +99,10 @@ basic_approach_flipped <- function(file){
       otu1_value <- table1[[otu1]][y]
       otu2_value <- table1[[otu2]][y]
       if(otu1_value > 0 & otu2_value > 0){
-        s <- otu1_value + otu2_value
-        return(s)
+        #s <- otu1_value + otu2_value
+        return(1)
       }else{
-        return (NULL)
+        return (0)
       }
     }))
     
@@ -91,6 +110,10 @@ basic_approach_flipped <- function(file){
     #debugging stuff
     if(x%%1000==0){
       print(x)
+    }
+    if(is.null(total)){
+      print(paste("Error! ",x," ",otu1," ",otu2))
+      return(NA)
     }
     #return count value
     return(total)
@@ -103,6 +126,8 @@ basic_approach_flipped <- function(file){
   return(result)
 }
 
+
+
 #create data table with all unique combinations, where (A,B) == (B,A)
 unique_combinations <- function(l1, l2){
   t<-as.data.table(expand.grid(l1,l2))
@@ -114,6 +139,6 @@ unique_combinations <- function(l1, l2){
 }
 
 
-basic_approach_flipped(file2)
+out<-basic_approach(file2)
 
 
