f5f8a85938a96f213d96bcfd24b6f1a3f7856496
Alex Dietrich <ga89noj@mytum.de>
2020-01-26 12:07:30 +0100

    Replace ui_alex.R

--- ui_alex.R
+++ ui_alex.R
@@ -99,11 +99,12 @@ ui <- dashboardPage(
                                 fluidRow(
                                   column(4,sliderInput("binCutoff","Cutoff for Binarization",0,10,1,step = 0.01)),
                                   column(4,plotlyOutput("cutoffHist")),
+                                  p("Heatmap of cutoff-effect: dark fields are being set to 0 in co-occurrence calculation."),
+                                  column(4,plotlyOutput("boolHeat")),
                                   column(2,radioButtons("useFC","Calculation of Counts:",c("log2(fold-change)","difference"))),
                                   column(2,selectInput("groupCol","Select Column from META-file containing groupings:",c()))),
                                 fluidRow(
                                   column(2,actionButton("startCalc","Start Count Calculation & Reload Network!"))
-                                  #,column(6,plotlyOutput("countDistr"))
                                 ),
                                 p("Network Plot"),
                                 fluidRow(
