170e156baf30c2e7f13718ac7dfa23e6a67b664c
alex-d13 <dietrich.alexander1310@gmail.com>
2021-08-13 09:31:28 +0200

    .

--- app/R/server/advanced_server.R
+++ app/R/server/advanced_server.R
@@ -520,7 +520,7 @@ observeEvent(input$picrust2Start,{
       
       if(!vals$datasets[[currentSet()]]$is_sample_data){
         picrust_outdir <- paste0(outdir,"/picrust2out")       # this is the name of the final output directory of this picrust run
-        command = paste0("/opt/anaconda3/bin/conda run -n picrust2 picrust2_pipeline.py --remove_intermediate -s ",fasta_file," -i ",biom_file, " -o ", picrust_outdir, " -p", ncores)
+        command = paste0("/opt/anaconda3/bin/conda run -n picrust2 picrust2_pipeline.py --remove_intermediate -s ",fasta_file," -i ",biom_file, " -o ", picrust_outdir, " -p", ncores*2)
         #command = paste0("/home/alex/anaconda3/bin/conda run -n picrust2 picrust2_pipeline.py --remove_intermediate -s ",fasta_file," -i ",biom_file, " -o ", picrust_outdir, " -p", ncores)
         message(paste0(Sys.time(), " - picrust2-command:"))
         message(command)
